58bf264b61dfd81938aaaeee69b0f37248636b05
NATASHA WILLIAMS <147554213+nwill86@users.noreply.github.com>
2024-05-28 16:12:52 -0400

    updated _layout.jsx with styling components

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>RootLayout</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default _layout
+
+const styles = StyleSheet.create({})
