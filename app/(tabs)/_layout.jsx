6702f412b6f3bda0fbd78a1ef9bf0a6140aa84dd
NATASHA WILLIAMS <147554213+nwill86@users.noreply.github.com>
2024-05-28 18:19:47 -0400

    updated tabs.

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import { Tabs, Redirect } from 'expo-router';
+
+const TabsLayout = () => {
+  return (
+    <>
+    <Tabs>
+   
+    </Tabs>
+    </>
+  )
+}
+
+export default TabsLayout
