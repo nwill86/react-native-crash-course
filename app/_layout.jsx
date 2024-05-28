24dcec109d736cc5e25ae5f8c6e69c662d976cc7
NATASHA WILLIAMS <147554213+nwill86@users.noreply.github.com>
2024-05-28 16:49:16 -0400

    Added code to link to newly created profile.jsx page

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Slot, Stack } from 'expo-router';
 
 const RootLayout = () => {
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
+    <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false}} />
+    </Stack>
   )
 }
 
-export default _layout
-
-const styles = StyleSheet.create({})
+export default RootLayout
