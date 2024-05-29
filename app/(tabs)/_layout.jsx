eef4b94724321316b0262a68c1d339502da6e8f9
NATASHA WILLIAMS <147554213+nwill86@users.noreply.github.com>
2024-05-29 12:23:27 -0400

    updated styling for tabs bar and images

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -24,7 +24,16 @@ const TabsLayout = () => {
     <>
     <Tabs
       screenOptions={{
-       tabBarShowLabel: false 
+       tabBarShowLabel: false,
+       tabBarActiveTintColor: '#FFA001',
+       tabBarInactiveTintColor: '#CDCDE0',
+       tabBarStyle: { 
+        backgroundColor: '#161622',
+        borderTopWidth:  1,
+        borderTopColor: '#232533',
+        height: 84,
+
+        }
       }}
     >
       <Tabs.Screen 
