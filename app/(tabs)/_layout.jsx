5d22c079fca0eeeccbfb2f458e892555448cbbc5
NATASHA WILLIAMS <147554213+nwill86@users.noreply.github.com>
2024-05-29 11:52:59 -0400

    added styling and image to home tab

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { Tabs, Redirect } from 'expo-router';
+
+import { icons } from '../../constants';
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View>
+      <Image 
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text>
+        {name}
+      </Text>
+    </View>
+  )
+}
 
 const TabsLayout = () => {
   return (
     <>
     <Tabs>
-   
-    </Tabs>
+      <Tabs.Screen 
+        name="home"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+              <TabIcon 
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+          )
+        }}
+      />
+      </Tabs>
     </>
   )
 }
